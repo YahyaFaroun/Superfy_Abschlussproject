c06c8217b9b8ee0a462d4244b917405bf1a60a36
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-08-14 10:25:56 +0200

    nochmal

--- src/Components/MusicList.js
+++ src/Components/MusicList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import MusicItem from './MusicItem';
 
 
-const url = "https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx%2C57dN52uHvrHOxijzpIgu3E%2C1vCWHaC5f2uS3yhpwWbIA6%2C3fMbdgg4jU18AjLCKBhRSm%2C6tbjWDEIzxoDsBA1FuhfPW%2C5pKCCKE2ajJHZ9KAiaK11H%2C2QsynagSdAqZj3U9HgDzjD"
+const url = "https://api.spotify.com/v1/artists?ids=2CIMQHirSU0MQqyYHq0eOx%2C57dN52uHvrHOxijzpIgu3E%2C1vCWHaC5f2uS3yhpwWbIA6%2C3fMbdgg4jU18AjLCKBhRSm%2C6tbjWDEIzxoDsBA1FuhfPW%2C5pKCCKE2ajJHZ9KAiaK11H%2C2QsynagSdAqZj3U9HgDzjD%2C3qm84nBOXUEQ2vnTfUTTFC%2C27T030eWyCQRmDyuvr1kxY%2C58lV9VcRSjABbAbfWS6skp%2C3WrFJ7ztbogyGnTHbHJFl2%2C22bE4uQ6baNwSHPVcDxLCe"
 const options = {
   headers: {
     Authorization: "Bearer BQDl3W7HzYkPBu7wVZg28E6LFA33D5OHFaAdQeu9TGqnor9bxRMhQz14_iLlV6uMc7763agLvFSGuat1H4Re_XRWcIFdPL_ocNhzsyBg6qByVFq19ViirN5DPldSjbsJj-B7Zg9Yaij7ehe_v90qKEsA920wkxrqUppXwDMT-rQpYphloLzPv5qg6RiwaYHhAGFhKEqDqM4GxqgZr8HG0NqZLFPUmHvr0VJoVzx_JdekcpIRunve0jPZ3WKKufb7oyMUAd0aOfY9QunD28lNbEWLLrqeiiAl"
