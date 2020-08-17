54056e4db0445082c27c3955fe8fc3c1654e2c75
iwinter74 <isabelle.winternazlisoz@googlemail.com>
2020-08-17 09:48:46 +0200

    push Montag1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import Footer from './Components/Footer/Footer'
 import Header from './Components/Header/Header';
 import MusicList from './Components/MusicList';
+import DetailedArtist from './Components/DetailedArtist';
+import './Components/Body.css';
+import Search from './Components/Search';
+import NoMatch from './Components/404';
 
       
 
-
-const App = () => {
+function App(props) {
   return ( 
+    <Router>
+      {props.children}
       <div>
-      <Header />
-      <MusicList />
-      <Footer />
+        <Header />
+        {/* <Search /> */}
+      <Switch>
+          <Route path="/" exact>
+          <MusicList />
+          </Route>
+          <Route path="/artists/:id" component={DetailedArtist} />
+          <Route>
+            <NoMatch />
+          </Route>
+      </Switch>
+        <Footer />
     </div>
+    </Router>
    );
 }
  
--- src/Components/MusicItem.js
+++ src/Components/MusicItem.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
+import {
+    Link
+} from "react-router-dom";
+
+
 
 const MusicItem = (props) => {
     return (
-        <div>
-            <img src={props.images[0].url} alt="" />
-            <h1>Artist: {props.name}</h1>
-            <p>Popularity: {props.popularity}</p>
+        <div className="music_container">
+            <div className={props.color}>
+                <Link to={`/artists/${props.id}`}><img src={props.images[0].url} alt="" /></Link>
+                <h1 className="artist_name">{props.name}</h1>
+                <p className="popularity">Popularity: {props.popularity}</p>
+                <button onClick={() => props.deleteContact(props.id)}>delete</button>
+            </div>
         </div>
       );
 }
